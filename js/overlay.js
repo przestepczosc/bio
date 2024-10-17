6ea14310cc1e1ae7e1fc7d9311f28de1a932732e
przestepczosc <151448269+przestepczosc@users.noreply.github.com>
2024-10-17 21:22:34 +0200

    Delete js/overlay.js

--- js/overlay.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-window.onload = function() {
-    const overlay = document.querySelector('.overlay');
-    overlay.style.opacity = 0;
-
-    setTimeout(() => {
-        overlay.style.display = 'none';
-    }, 2000);
-}});
